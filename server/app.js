9057fc7cd08018700e0ae1c6b6befa8332362626
enjoymrban <knecht.silvan@gmail.com>
2019-03-12 20:41:32 +0100

    adding routes in server

--- server/app.js
+++ server/app.js
@@ -15,6 +15,7 @@ app.use(express.json());
 
 // Routes
 app.use('/users', require('./routes/users'));
+app.use('/tasks', require('./routes/tasks'));
 
 // Start the server
 const port = process.env.PORT || 5000;
