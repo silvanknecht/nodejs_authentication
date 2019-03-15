b5a962744619ab2ec05301251ae419cce06ad3a8
Silvan Knecht <knecht.silvan@gmail.com>
2019-03-15 18:11:34 +0100

    added swagger

--- server/app.js
+++ server/app.js
@@ -2,22 +2,29 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
-// Database
+// swagger
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
+// database
 mongoose.connect('mongodb://localhost/APIAuthentication', { useNewUrlParser: true });
 mongoose.set('useCreateIndex', true); // Without it Deprication Warning
 
 const app = express();
 
 
-// Middleware
+// middleware
 app.use(morgan('dev'));
 app.use(express.json());
 
-// Routes
+// routes
+// users
 app.use('/users', require('./routes/users'));
-app.use('/tasks', require('./routes/tasks'));
 
-// Start the server
+// swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`listening on port ${port}...`);
