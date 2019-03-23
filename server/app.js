a0eb90384dbd8da11be70fa237be044c9df213ca
enjoymrban <knecht.silvan@gmail.com>
2019-03-23 13:57:10 +0100

    make prettier

--- server/app.js
+++ server/app.js
@@ -1,30 +1,26 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const debugAuthServer = require('debug')('auth:server');
-
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const debugAuthServer = require("debug")("auth:server");
 
 // database
-mongoose.connect('mongodb://localhost/APIAuthentication', {
-    useNewUrlParser: true
+mongoose.connect("mongodb://localhost/APIAuthentication", {
+  useNewUrlParser: true
 });
-mongoose.set('useCreateIndex', true); // Without it Deprication Warning
+mongoose.set("useCreateIndex", true); // Without it Deprication Warning
 
 const app = express();
 
-
 // middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-
 
 // routes
 // users
-app.use('/api/v1/users', require('./routes/users'));
-
+app.use("/api/v1/users", require("./routes/users"));
 
 // start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    debugAuthServer(`listening on port ${port}...`);
+  debugAuthServer(`listening on port ${port}...`);
 });
--- server/models/user.js
+++ server/models/user.js
@@ -1,96 +1,88 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
-const debugUserModel = require('debug')('auth:userModel');
-
-
-
+const debugUserModel = require("debug")("auth:userModel");
 
 // Create a schema
 // TODO: Add other fields: lastname, firstname, birthday, etc.
 const userSchema = new Schema({
-    // Additional validation, it's also done serverside!
-    methode: {
-        type: String,
-        enum: ['local', 'google', 'facebook', 'github'],
-        required: true
+  // Additional validation, it's also done serverside!
+  methode: {
+    type: String,
+    enum: ["local", "google", "facebook", "github"],
+    required: true
+  },
+  local: {
+    email: {
+      type: String,
+      lowercase: true
     },
-    local: {
-        email: {
-            type: String,
-            lowercase: true
-        },
-        password: {
-            type: String
-        }
-    },
-    google: {
-        id: {
-            type: String
-        },
-        email: {
-            type: String,
-            lowercase: true
-        }
-    },
-    facebook: {
-        id: {
-            type: String
-        },
-        email: {
-            type: String,
-            lowercase: true
-        }
-    },
-    github: {
-        id: {
-            type: String
-        },
-        email: {
-            type: String,
-            lowercase: true
-        }
+    password: {
+      type: String
     }
-
-
+  },
+  google: {
+    id: {
+      type: String
+    },
+    email: {
+      type: String,
+      lowercase: true
+    }
+  },
+  facebook: {
+    id: {
+      type: String
+    },
+    email: {
+      type: String,
+      lowercase: true
+    }
+  },
+  github: {
+    id: {
+      type: String
+    },
+    email: {
+      type: String,
+      lowercase: true
+    }
+  }
 });
 
 // Before the new use is saved in the database
-userSchema.pre('save', async function (next) {
-//console.log('this.logal.password', this.local.password);
-    try {
-        if(this.methode !== 'local'){
-            next();
-        }
-
-        // generate a salt
-        const salt = await bcrypt.genSalt(10);
-        // generate  password has (salt +hash)
-        const passowrdHas = await bcrypt.hash(this.local.password, salt) //userpassoword, salt => contains hash and hashedPassword, with that hash can the entered password while login be comapred
-        // assign hashed Password
-        this.local.password = passowrdHas;
-        next();
-    } catch (error) {
-        debugUserModel('Save user model in DB failed');
-        next(error);
+userSchema.pre("save", async function(next) {
+  //console.log('this.logal.password', this.local.password);
+  try {
+    if (this.methode !== "local") {
+      next();
     }
+
+    // generate a salt
+    const salt = await bcrypt.genSalt(10);
+    // generate  password has (salt +hash)
+    const passowrdHas = await bcrypt.hash(this.local.password, salt); //userpassoword, salt => contains hash and hashedPassword, with that hash can the entered password while login be comapred
+    // assign hashed Password
+    this.local.password = passowrdHas;
+    next();
+  } catch (error) {
+    debugUserModel("Save user model in DB failed");
+    next(error);
+  }
 }); // before user gets saved this is executed, ES6 arrow functions don't work when referencing out of this object
 
 // before logged in we need to check whether or not the password is correct
-userSchema.methods.isValidPassword = async function (passwordToCheck) {
-    try {
-        return await bcrypt.compare(passwordToCheck, this.local.password);
-
-    } catch (error) {
-        debugUserModel('Password was rejected');
-        throw new Error(error);
-
-    }
-
-}
+userSchema.methods.isValidPassword = async function(passwordToCheck) {
+  try {
+    return await bcrypt.compare(passwordToCheck, this.local.password);
+  } catch (error) {
+    debugUserModel("Password was rejected");
+    throw new Error(error);
+  }
+};
 
 // Create a model
-const User = mongoose.model('user', userSchema); // name will be pluralized automatically for DB
+const User = mongoose.model("user", userSchema); // name will be pluralized automatically for DB
 
 // Export the model
 module.exports = User;
